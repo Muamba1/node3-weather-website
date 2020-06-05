6a2ecd0de1a80c05223c4e5d34880c4282a35f51
Moise Muamba <moisemuamba@Elsas-MacBook-Pro.local>
2020-06-05 17:20:50 +0100

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,27 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) =>{
+
+    const url = 'http://api.weatherstack.com/current?access_key=3c6478fdfb326b7e345ade045e862ebe&query=' + latitude + ',' + longitude + '&units=m'
+
+    request({url, json: true}, (error, { body }) =>{
+
+        if (error){
+
+            callback ('Unable to connect to weather service', undefined)
+
+        } else if (body.error){
+
+            callback ('Unable to find location', undefined)
+
+        } else {
+
+            callback ('undefined', body.current.weather_descriptions[0]+'. It is currently ' +body.current.temperature+ ' degrees out. There is ' +body.current.precip+ ' % chance of rain')
+
+        }
+
+    })
+
+}
+
+module.exports = forecast 
